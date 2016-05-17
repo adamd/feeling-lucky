46722861e3c6a031f59df5d9133ea44d58381111
Adam DuVander <adam@orchestrate.io>
2016-05-17 12:14:24 -0700

    Search lookup on terms in CSV file

--- lucky.py
+++ lucky.py
@@ -1,4 +1,26 @@
-import sys, urllib, requests
+import sys, urllib, requests, csv
+
+# Find the top result for search terms in CSV
+def process_file(fn):
+    results = []
+    with open(fn, 'rb') as f:
+        rd = csv.reader(f)
+        header = rd.next()
+        results.append(header)
+        for row in rd:
+            row[1] = lucky_search(row[0])
+            results.append(row)
+    f.close()
+    write_file(fn, results)
+    print "Processed " + str(len(results)-1) + " search terms"
+
+# Save the search results in CSV
+def write_file(fn, results):
+    with open(fn, 'wb') as f:
+        wt = csv.writer(f)
+        for row in results:
+            wt.writerow(row)
+    f.close()
 
 # Use Google's I'm Feeling Lucky option to retrieve the top searh result
 def lucky_search(q):
@@ -7,11 +29,10 @@ def lucky_search(q):
 
     headers = {'user-agent': user_agent}
     r = requests.get(url, headers=headers, allow_redirects=True)
-    print r.url
+    return r.url
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print "Usage: lucky.py filename.csv"
     else:
-        #process_file(sys.argv[1])
-        lucky_search(sys.argv[1])
+        process_file(sys.argv[1])
